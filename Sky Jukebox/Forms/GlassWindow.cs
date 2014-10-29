421073d317f34ba8aed82b3eb51ba966055f0b0d
OronDF343 <orondf343@gmail.com>
2014-10-29 13:40:07 +0200

    More refactoring for MiniPlayer
    It is now possible to create more glass windows!

---
+++ Sky Jukebox/Forms/GlassWindow.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace SkyJukebox.Forms
+{
+    public abstract class GlassWindow : Window
+    {
+        protected GlassWindow()
+        {
+            Loaded += GlassWindow_Loaded;
+            SourceInitialized += GlassWindow_SourceInitialized;
+            MouseDown += GlassWindow_MouseDown;
+        }
+        public bool DisableAeroGlass { get; set; }
+        private HwndSource _mainWindowSrc;
+        private void GlassWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DisableAeroGlass) return;
+            var windowInteropHelper = new WindowInteropHelper(this);
+            var handle = windowInteropHelper.Handle;
+            _mainWindowSrc = HwndSource.FromHwnd(handle);
+
+            if (_mainWindowSrc == null || _mainWindowSrc.CompositionTarget == null)
+            {
+                MessageBox.Show("Error getting HwndSource! Window appearance can't be properly set!", "Exception in Loaded", MessageBoxButton.OK,
+                    MessageBoxImage.Asterisk);
+                return;
+            }
+            _mainWindowSrc.CompositionTarget.BackgroundColor = Colors.Transparent;
+            var glassParams = new NativeMethods.DwmBlurbehind
+            {
+                dwFlags = NativeMethods.DwmBlurbehind.DWM_BB_ENABLE,
+                fEnable = true,
+                hRegionBlur = IntPtr.Zero
+            };
+
+            if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor < 2)
+            {
+                var dis = 2;
+                NativeMethods.DwmSetWindowAttribute(_mainWindowSrc.Handle,
+                    NativeMethods.DwmWindowAttribute.DWMWA_LAST,
+                    ref dis,
+                    sizeof(uint));
+            }
+            NativeMethods.DwmEnableBlurBehindWindow(
+                handle,
+                glassParams);
+        }
+
+        private void GlassWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            var source = (HwndSource)PresentationSource.FromVisual(this);
+            if (source == null)
+            {
+                MessageBox.Show("Error getting HwndSource! Window appearance can't be properly set!", "Exception in SourceInitialized", MessageBoxButton.OK,
+                    MessageBoxImage.Asterisk);
+                return;
+            }
+            source.AddHook(HwndSourceHook);
+            var hWnd = new WindowInteropHelper(this).Handle;
+            var flags = NativeMethods.GetWindowLongPtr(hWnd, -16 /*GWL_STYLE*/);
+#if WIN32
+            var dwnl = flags & ~(0x00010000 /*WS_MAXIMIZEBOX*/| 0x00020000 /*WS_MINIMIZEBOX*/| 0x00080000 /*WS_SYSMENU*/);
+#else
+            var dwnl = new IntPtr(flags.ToInt64() & ~(0x00010000L /*WS_MAXIMIZEBOX*/| 0x00020000L /*WS_MINIMIZEBOX*/| 0x00080000L /*WS_SYSMENU*/));
+#endif
+            NativeMethods.SetWindowLongPtr(hWnd, -16 /*GWL_STYLE*/, dwnl);
+        }
+
+        protected virtual IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0084) /*WM_NCHITTEST*/
+            {
+                var result = NativeMethods.DefWindowProc(hwnd, msg, wParam, lParam);
+                if (result.ToInt32() >= 10 /*HTLEFT*/ && result.ToInt32() <= 17 /*HTBOTTOMRIGHT*/ )
+                {
+                    handled = true;
+                    return new IntPtr(18 /*HTBORDER*/);
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        // Drag the window:
+        private void GlassWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left)
+                return;
+            NativeMethods.ReleaseCapture();
+            NativeMethods.SendMessage(_mainWindowSrc.Handle, NativeMethods.WmNclbuttondown, new IntPtr(NativeMethods.HtCaption), new IntPtr(0));
+        }
+    }
+}
--- Sky Jukebox/MiniPlayer.xaml.cs
+++ Sky Jukebox/MiniPlayer.xaml.cs
@@ -6,8 +6,6 @@ using System.Globalization;
 using System.IO;
 using System.Windows;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using SkyJukebox.PluginAPI;
@@ -34,6 +32,7 @@ namespace SkyJukebox
         private NotifyIcon _controlNotifyIcon;
         public MiniPlayer()
         {
+            DisableAeroGlass = Settings.Instance.DisableAeroGlass;
             InitializeComponent();
             InitNotifyIcon();
 
@@ -55,7 +54,6 @@ namespace SkyJukebox
                     Settings.Instance.GuiColor.G, Settings.Instance.GuiColor.B));
             }
 
-            Background = Brushes.Transparent;
             SetProgressColor(Settings.Instance.ProgressColor);
             SetBgColor(Settings.Instance.BgColor);
         }
@@ -211,9 +209,6 @@ namespace SkyJukebox
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Activate aero glass
-            ActivateAeroGlass();
-
             if (Settings.Instance.ShowPlaylistEditorOnStartup)
             {
                 var pe = new PlaylistEditor();
@@ -269,44 +264,6 @@ namespace SkyJukebox
             return true;
         }
 
-        #region Aero Glass
-        private HwndSource _mainWindowSrc;
-        private void ActivateAeroGlass()
-        {
-            if (Settings.Instance.DisableAeroGlass) return;
-            var windowInteropHelper = new WindowInteropHelper(this);
-            var handle = windowInteropHelper.Handle;
-            _mainWindowSrc = HwndSource.FromHwnd(handle);
-
-            if (_mainWindowSrc == null || _mainWindowSrc.CompositionTarget == null)
-            {
-                MessageBox.Show("Error getting HwndSource! Window appearance can't be properly set!", "Exception in Loaded", MessageBoxButton.OK,
-                    MessageBoxImage.Asterisk);
-                return;
-            }
-            _mainWindowSrc.CompositionTarget.BackgroundColor = Colors.Transparent;
-            var glassParams = new NativeMethods.DwmBlurbehind
-            {
-                dwFlags = NativeMethods.DwmBlurbehind.DWM_BB_ENABLE,
-                fEnable = true,
-                hRegionBlur = IntPtr.Zero
-            };
-
-            if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor < 2)
-            {
-                var dis = 2;
-                NativeMethods.DwmSetWindowAttribute(_mainWindowSrc.Handle,
-                    NativeMethods.DwmWindowAttribute.DWMWA_LAST,
-                    ref dis,
-                    sizeof(uint));
-            }
-            NativeMethods.DwmEnableBlurBehindWindow(
-                handle,
-                glassParams);
-            //DwmApi.DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, new DwmApi.Margins(0, 0, 0, 0));
-        }
-        #endregion
-
         #region Scrolling Text
         string _currentText;
         private void SetTextScrollingAnimation(string text)
@@ -339,39 +296,10 @@ namespace SkyJukebox
         }
         #endregion
 
-        #region Disable resizing the hacky way
+        #region Single instance handling
 
-        private void Window_SourceInitialized(object sender, EventArgs e)
+        protected override IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            var source = (HwndSource)PresentationSource.FromVisual(this);
-            if (source == null)
-            {
-                MessageBox.Show("Error getting HwndSource! Window appearance can't be properly set!", "Exception in SourceInitialized", MessageBoxButton.OK,
-                    MessageBoxImage.Asterisk);
-                return;
-            }
-            source.AddHook(HwndSourceHook);
-            var hWnd = new WindowInteropHelper(this).Handle;
-            var flags = NativeMethods.GetWindowLongPtr(hWnd, -16 /*GWL_STYLE*/);
-#if WIN32
-            var dwnl = flags & ~(0x00010000 /*WS_MAXIMIZEBOX*/| 0x00020000 /*WS_MINIMIZEBOX*/| 0x00080000 /*WS_SYSMENU*/);
-#else
-            var dwnl = new IntPtr(flags.ToInt64() & ~(0x00010000L /*WS_MAXIMIZEBOX*/| 0x00020000L /*WS_MINIMIZEBOX*/| 0x00080000L /*WS_SYSMENU*/));
-#endif
-            NativeMethods.SetWindowLongPtr(hWnd, -16 /*GWL_STYLE*/, dwnl);
-        }
-
-        private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (msg == 0x0084) /*WM_NCHITTEST*/
-            {
-                var result = NativeMethods.DefWindowProc(hwnd, msg, wParam, lParam);
-                if (result.ToInt32() >= 10 /*HTLEFT*/ && result.ToInt32() <= 17 /*HTBOTTOMRIGHT*/ )
-                {
-                    handled = true;
-                    return new IntPtr(18 /*HTBORDER*/);
-                }
-            }
             // Single instance: handling the message
             if (msg == ((App)Application.Current).Message)
                 {
@@ -382,18 +310,7 @@ namespace SkyJukebox
                     if (!LoadFileFromClArgs())
                         MessageBox.Show("Failed to load file: returned false", "Debug", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 }
-            return IntPtr.Zero;
-        }
-
-        // Drag the window:
-
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton != MouseButton.Left)
-                return;
-            NativeMethods.ReleaseCapture();
-            NativeMethods.SendMessage(_mainWindowSrc.Handle, NativeMethods.WmNclbuttondown, new IntPtr(NativeMethods.HtCaption), new IntPtr(0));
+            return base.HwndSourceHook(hwnd, msg, wParam, lParam, ref handled);
         }
         #endregion
 
@@ -467,7 +384,6 @@ namespace SkyJukebox
                     break;
             }
         }
-
         private void openPlaylistButton_Click(object sender, RoutedEventArgs e)
         {
             DoFocusChange();
